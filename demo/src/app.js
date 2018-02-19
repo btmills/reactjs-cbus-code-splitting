74ebe65dd04dd087b53aa2a5285646414588751a
Brandon Mills <mills.brandont@gmail.com>
2018-02-19 16:41:26 -0500

    Hello world app

---
+++ demo/src/app.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const App = () => (
+	<h1>Hello from React!</h1>
+);
+
+export default App;
---
+++ demo/webpack.config.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: 'src/index.html'
+		})
+	],
+	module: {
+		rules: [{
+			test: /\.js$/,
+			include: path.resolve(__dirname, 'src'),
+			use: {
+				loader: 'babel-loader',
+				options: {
+					presets: [
+						['@babel/preset-env', {
+							modules: false,
+							targets: {
+								chrome: 64
+							},
+							useBuiltIns: 'usage'
+						}],
+						'@babel/preset-react'
+					]
+				}
+			}
+		}]
+	}
+};
