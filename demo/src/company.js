9de3475bae94586b841706e35d60e311a37b6fde
Brandon Mills <mills.brandont@gmail.com>
2018-02-20 23:20:22 -0500

    Add datamap

--- demo/src/company.js
+++ demo/src/company.js
@@ -1,7 +1,37 @@
 import React from 'react';
+import Datamap from 'react-datamaps';
 
-const Company = ({ match }) => (
-	<h1>Profile {match.params.id}</h1>
-);
+import { companies } from './database';
+
+const Company = ({ match }) => {
+	const {
+		name,
+		coordinates: [latitude, longitude],
+		employees
+	} = companies[match.params.id];
+
+	return (
+		<div>
+			<h1>Profile {match.params.id}</h1>
+			<Datamap
+				bubbles={[{
+					fillKey: 'bubble',
+					name,
+					latitude,
+					longitude,
+					radius: 20
+				}]}
+				fills={{
+					bubble: '#4286f4',
+					defaultFill: '#d5dce8'
+				}}
+				height={400}
+				labels
+				scope="usa"
+				width={600}
+			/>
+		</div>
+	);
+};
 
 export default Company;
