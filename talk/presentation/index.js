d7292258666899ddee8b33ba5ef2852d8f811442
Brandon Mills <mills.brandont@gmail.com>
2018-02-24 15:35:41 -0500

    Add spectacle-code-slide

--- talk/presentation/index.js
+++ talk/presentation/index.js
@@ -1,84 +1,68 @@
-// Import React
-import React from "react";
-
-// Import Spectacle Core tags
+import React from 'react';
 import {
-	BlockQuote,
-	Cite,
 	Deck,
 	Heading,
-	ListItem,
-	List,
-	Quote,
 	Slide,
 	Text
-} from "spectacle";
-
-// Import theme
-import createTheme from "spectacle/lib/themes/default";
-
-import preload from "spectacle/lib/utils/preloader";
+} from 'spectacle';
+import createTheme from 'spectacle/lib/themes/default';
+import preload from 'spectacle/lib/utils/preloader';
+import CodeSlide from 'spectacle-code-slide';
 
 // Require CSS
-require("normalize.css");
-
-const theme = createTheme({
-	primary: "#1F2022",
-	secondary: "white",
-	tertiary: "#03A9FC",
-	quarternary: "#CECECE"
-}, {
-	primary: "Montserrat",
-	secondary: "Helvetica"
-});
+require('normalize.css');
 
 const images = {
 	// Photo by Igor Ovsyannykov on Unsplash
-	splash: require("../assets/splash.jpg").replace("/", "")
+	splash: require('../assets/splash.jpg').replace('/', '')
 };
-
 preload(images);
+
+const theme = createTheme({
+	primary: 'white',
+	secondary: '#1F2022',
+	tertiary: '#03A9FC',
+	quarternary: '#CECECE'
+	// primary: '#011627',
+	// secondary: '#B7CECE',
+	// tertiary: '#B7CECE'
+}, {
+	primary: 'Montserrat',
+	secondary: 'Helvetica'
+});
 
 export default class Presentation extends React.Component {
 	render() {
 		return (
 			<Deck
+				progress="none"
 				theme={theme}
-				transition={["zoom", "slide"]}
-				transitionDuration={500}
+				transition={['fade']}
+				transitionDuration={250}
 			>
-				<Slide transition={["zoom"]} bgColor="primary" /*bgImage={images.splash} bgDarken={0.6}*/>
-					<Heading size={1} fit lineHeight={1} textColor="secondary">
+				<Slide>
+					<Heading
+						bold={false}
+						lineHeight={1.3}
+					>
 						Code-Splitting a React App
 					</Heading>
-					{/* <Text margin="10px 0 0" textColor="tertiary" size={1} fit bold>
-						open the presentation/index.js file to get started
-					</Text> */}
 				</Slide>
-				<Slide transition={["fade"]} bgColor="tertiary">
-					<Heading size={6} textColor="primary" caps>Typography</Heading>
-					<Heading size={1} textColor="secondary">Heading 1</Heading>
-					<Heading size={2} textColor="secondary">Heading 2</Heading>
-					<Heading size={3} textColor="secondary">Heading 3</Heading>
-					<Heading size={4} textColor="secondary">Heading 4</Heading>
-					<Heading size={5} textColor="secondary">Heading 5</Heading>
-					<Text size={6} textColor="secondary">Standard text</Text>
-				</Slide>
-				<Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
-					<Heading size={6} textColor="secondary" caps>Standard List</Heading>
-					<List>
-						<ListItem>Item 1</ListItem>
-						<ListItem>Item 2</ListItem>
-						<ListItem>Item 3</ListItem>
-						<ListItem>Item 4</ListItem>
-					</List>
-				</Slide>
-				<Slide transition={["fade"]} bgColor="secondary" textColor="primary">
-					<BlockQuote>
-						<Quote>Example Quote</Quote>
-						<Cite>Author</Cite>
-					</BlockQuote>
-				</Slide>
+				<CodeSlide
+					lang="js"
+					code={require('raw-loader!../assets/webpack.example')}
+					ranges={[
+						{ loc: [0, 999], title: 'Webpack config' },
+						{ loc: [5, 8] },
+						{ loc: [8, 13] },
+						{ loc: [13, 32] },
+						{ loc: [15, 16] },
+						{ loc: [16, 17] },
+						{ loc: [18, 19] },
+						{ loc: [21, 26] },
+						{ loc: [26, 27] }
+					]}
+				/>
 			</Deck>
 		);
 	}
